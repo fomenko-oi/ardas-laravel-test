f44f44c89c3087ecd09592711bafbc4d8d06291e
truehero <fomenko.a@i.ua>
2021-02-28 00:50:56 +0200

    Remove user api endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,6 @@
 use App\Http\Actions\Product\Create as CreateProductAction;
 use App\Http\Actions\Product\Search as SearchProductsAction;
 use App\Http\Actions\Product\Update as UpdateProductAction;
-use Illuminate\Http\Request;
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::post('products', [CreateProductAction::class, 'execute']);
 Route::put('products/{product}', [UpdateProductAction::class, 'execute']);
